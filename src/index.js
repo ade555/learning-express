30f00cfd990c2d85a7737c05f4c5819431816e1e
superherojt <ademolathompson5@gmail.com>
2024-04-16 15:38:12 +0100

    cookies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import express from "express";
 import usersRouters from "./routes/users.js";
 import productRouters from "./routes/products.js";
+import cookieParser from "cookie-parser";
 
 // initialize express app
 const app = express();
+const cookieSecret = "9784yhrjkkj8dmwdk.";
 
 // content type to json
 app.use(express.json());
 
+app.use(cookieParser(cookieSecret));
 app.use(usersRouters);
 app.use(productRouters);
 
 const PORT = process.env.PORT || 3000;
+
+export const helloCookieValue = "testcookie4283849hnd3ii";
+app.get('/', (req, res)=>{
+    res.cookie('hello', helloCookieValue, {maxAge:60000, signed:true});
+    res.send({"msg":"cookie set"});
+});
 
 app.listen(PORT, ()=>console.log("server started"));
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { helloCookieValue } from "../index.js";
 
 const router = Router();
 
 router.get("/api/products/", (req, res)=>{
-    res.status(200).send({"message":"this is the product page"});
+    console.log(req.signedCookies);
+    if (req.signedCookies.hello && req.signedCookies.hello=== helloCookieValue) return res.status(200).send({"message":"this is the product page"});
+    return res.status(200).send({"message":"no cookie set"});
 });
 
 export default router;
